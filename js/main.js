d27bec95354b2e9645e32aba14acc7853beae05d
Dylan <dylan.cobean@gmail.com>
2016-02-05 00:16:29 -0700

    Improved color pallet and font

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
+$(document).ready(function() {
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+/*
 //Removes any focus on a button after it was clicked
 //I implemented this, because when you click on the resume Modal, it remains 'focused' and I either write this code in, or disable all focus css for that button. This was easier
-$(a).mouseup(function(){
-    $(this).blur();
+
+function enterexit(e) {
+    $("e.currentTarget").removeClass('entered').addClass('exited');
+}
+
+
+$("#writings").mouseenter(function(){
+    $(Add).blur();
 })
+
+mouseenter(function(event) {
+	$(addClass('mouseenterportfolio'))
+});
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
