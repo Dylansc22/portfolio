3a13fa1bd9d65b82a31b64f6d4926a1625002648
Dylan <dylan.cobean@gmail.com>
2016-02-19 22:30:51 -0700

    major update, tabs and scroll completely working

--- js/main.js
+++ js/main.js
@@ -11,8 +11,22 @@ $(document).ready(function() {
         $(this).tab('show');
     })
 
+/*When affixing the navbar to the top of the page, The element being affixed is being pulled out 
+of the dom and positioned by our JS basically. Normally, the nav there would just take up the 
+width of its parent, but it effectively doesn't have one once affixed. 
+You'll need to set a width for the element manually. And this code does just that*/
+/*Pulled from https://github.com/twbs/bootstrap/issues/6350*/
+function affixWidth() {
+    var affix = $('#affixeditem');
+    var width = affix.width();
+    affix.width(width);
+}
 
+$(document).ready(function () {
 
+    affixWidth();
+
+});
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
